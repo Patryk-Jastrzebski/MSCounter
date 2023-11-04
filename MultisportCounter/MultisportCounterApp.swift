4ba943fb17f8cfd1aba0e92606496b4db4f5479e
Patryk Jastrzebski <patryk.jastrzebski@iteo.com>
2023-11-04 11:35:03 +0100

    Initial Commit

---
+++ MultisportCounter/MultisportCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultisportCounterApp.swift
+//  MultisportCounter
+//
+//  Created by Patryk Jastrzębski on 04/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultisportCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
